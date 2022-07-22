edb2d88ea1996067d54578c7186a2d4580187a7c
Shmuel Hoffman <snyh212@gmail.com>
2022-07-21 17:46:15 -0700

    linked index.js to lib files and started inquirer prompt

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const Employee = require("./lib/employee");
+const inquirer = require("inquirer");
+const fs = require('fs');
 
-const employee = new Employee()
+
+const Employee = require("./lib/employee");
+const manager = require("/manager");
+const engineer = require("/engineer");
+const intern = require("/intern");
+
+//variable to hold answer data
+const answerData = [];
+
+const questions = async () => {
+    const answers = await inquirer
+    .prompt([{
+        
+    }])
+}
 
 employee.create();

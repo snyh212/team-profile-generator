55ed6f3d88fd99db328d441ec14610bec731194f
Shmuel Hoffman <snyh212@gmail.com>
2022-07-21 18:04:53 -0700

    added manager questions prompt and pushed data to answers array

--- index.js
+++ index.js
@@ -12,9 +12,39 @@ const answerData = [];
 
 const questions = async () => {
     const answers = await inquirer
-    .prompt([{
-        
-    }])
+    .prompt([
+        {
+            type: "list",
+            message: "what is your role?",
+            name: "role",
+            choices: ["Manager", "Engineer", "Intern"]
+        },
+        {
+            type: "input",
+            message: "what is your name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "what is your employee ID numer?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "what is your E-mail address?",
+            name: "email",
+        },
+    ])
+    if (answers.role === "Manager") {
+        const managerAnswers = await inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "what is your office numer?",
+                name: "office",
+            },
+        ])
+    }
 }
 
 employee.create();

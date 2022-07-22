1904568b2e308a910f07e43d54ca567c15d0d7ff
Shmuel Hoffman <snyh212@gmail.com>
2022-07-21 20:32:46 -0700

    added test for intern

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,14 @@
-const intern = require("../lib/intern");
+const Intern = require("../lib/intern");
+
+describe("Intern", () => {
+    describe("getinfo", () => {
+        it("should return UCB", () =>{
+            const name = "yed";
+            const id = "99";
+            const email = "sn@gmail.com";
+            const school = "UCB";
+            const example = new Intern(name, id, email, school).getSchool(school);
+            expect(example).toEqual(school)
+        } )
+    })
+})

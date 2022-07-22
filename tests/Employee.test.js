9b0052b863094014652fd61c732604c334ca9884
Shmuel Hoffman <snyh212@gmail.com>
2022-07-21 20:35:44 -0700

    added 2 more tests in employee test js

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -9,5 +9,19 @@ describe("Employee", () => {
             const example = new Employee(name, id, email).getName(name);
             expect(example).toEqual(name)
         } )
+        it("should return 99", () =>{
+            const name = "yed";
+            const id = "99";
+            const email = "sn@gmail.com";
+            const example = new Employee(name, id, email).getId(id);
+            expect(example).toEqual(id)
+        } )
+        it("should return sn@gmail.com", () =>{
+            const name = "yed";
+            const id = "99";
+            const email = "sn@gmail.com";
+            const example = new Employee(name, id, email).getEmail(email);
+            expect(example).toEqual(email)
+        } )
     })
 })
